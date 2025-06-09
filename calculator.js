6ad22944e06cb1bd496658d9543954a65846a700
Mugdho <muhammadmugdho@gmail.com>
2025-06-10 04:14:04 +0600

    Created a BMI Calculator inside the route '../bmicalculator' that gives the proper Body Mass Index value

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
@@ -13,6 +14,19 @@ app.post('/', (req, res) => {
     var num2 = Number(req.body.num2);
     var result = num1 + num2;
     res.send("The result of the calculation is: " + result);
+});
+
+
+
+app.get('/bmicalculator', (req, res) => {
+    res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post('/bmicalculator', (req, res) => {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight / (height * height);
+    res.send("Your BMI is " + bmi);
 });
 
 app.listen(3000, function() {
